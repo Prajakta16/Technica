b7bd5291130599fd944d47d411213975dcb06c6f
chitali <chitalib@gmail.com>
2020-10-24 10:10:35 -0700

    inital template

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get(['/home', '/'], function (req, res) {
+    res.render('home',{
+      oscillator: false,
+    instrument: false
+    });
+});
+
+app.get('/chat', function (req, res) {
+  res.render('chat', {
+    oscillator: true,
+    instrument: false
+  });
+
+});
+app.get('*', function (req, res) {
+    res.render('404');
+    res.status(404);
+  
+  });
+
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+});
