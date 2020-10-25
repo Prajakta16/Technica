bd8a3533dfb7c56c8f6d773a106681d85f1a86a6
chitali <chitalib@gmail.com>
2020-10-24 18:34:45 -0700

    read from json file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-
+var allMessages = require('./messages.json');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -8,21 +10,20 @@ app.engine('handlebars', exphbs({defaultLayout:'main'}));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+app.use(express.json());
+app.use(bodyParser.json());
 
 app.get(['/home', '/'], function (req, res) {
-    res.render('home',{
-      oscillator: false,
-    instrument: false
-    });
 });
 
 app.get('/chat', function (req, res) {
-  res.render('chat', {
-    oscillator: true,
-    instrument: false
+  allMessages.push(req.body)
+  fs.writeFileSync('./messages.json', JSON.stringify(allMessages));
+  res.status(200).render('chat', {
+    MData: allMessages,
   });
-
 });
+
 app.get('*', function (req, res) {
     res.render('404');
     res.status(404);
